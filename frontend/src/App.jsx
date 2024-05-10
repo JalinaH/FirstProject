b78bf12b61e669f75a1260b45062db27302bd705
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-10 07:23:20 +0530

    chore: Update button in App.jsx to navigate to /users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div>
         <h1>Welcome to my webpage</h1>
-        <button className="user-button">Users</button>
+        <button className="user-button" onClick={() => navigate("/users")}>
+          Users
+        </button>
       </div>
     </>
   );
