cd601ac07d575e846f602acd353fa290d065b4dd
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-10 07:49:11 +0530

    chore: Refactor UsersTable component to render dynamic user data

--- frontend/src/UsersTable.jsx
+++ frontend/src/UsersTable.jsx
@@ -1,39 +1,58 @@
-import { Paper, Table, TableBody, TableContainer } from "@mui/material";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  Button,
+} from "@mui/material";
 import React from "react";
 
 const UsersTable = ({ rows }) => {
-  <TableContainer component={Paper}>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>ID</TableCell>
-          <TableCell>Name</TableCell>
-          <TableCell>Actions</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row) => (
-          <TableRow
-            key={row.id}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {row.id}
-            </TableCell>
-            <TableCell component="th" scope="row">
-              {row.name}
-            </TableCell>
-            <TableCell>
-              <Button sx={{ margin: "0px 10px" }} onclick={() => {}}>
-                Edit
-              </Button>
-              <Button sx={{ margin: "0px 10px" }} onclick={() => {}}>
-                Delete
-              </Button>
-            </TableCell>
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Actions</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>;
+        </TableHead>
+        <TableBody>
+          {rows.length > 0 ? (
+            rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>
+                  <Button sx={{ margin: "0px 10px" }} onClick={() => {}}>
+                    Edit
+                  </Button>
+                  <Button sx={{ margin: "0px 10px" }} onClick={() => {}}>
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell component="th" scope="row">
+                No data
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
