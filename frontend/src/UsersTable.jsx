48b55806112f50fbc0e66b8b3298b1e565b26f9e
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-10 07:43:17 +0530

    chore: Update UsersTable component to render dynamic user data

--- frontend/src/UsersTable.jsx
+++ frontend/src/UsersTable.jsx
@@ -1,20 +1,39 @@
-import { Paper, Table, TableBody, TableContainer } from '@mui/material';
-import React from 'react';
+import { Paper, Table, TableBody, TableContainer } from "@mui/material";
+import React from "react";
 
-const UsersTable = () => {
-    <TableContainer component={Paper}>
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>ID</TableCell>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Actions</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                
-            </TableBody>
-        </Table>
-    </TableContainer>
-
-}
+const UsersTable = ({ rows }) => {
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>ID</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Actions</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow
+            key={row.id}
+            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+          >
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell>
+              <Button sx={{ margin: "0px 10px" }} onclick={() => {}}>
+                Edit
+              </Button>
+              <Button sx={{ margin: "0px 10px" }} onclick={() => {}}>
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>;
+};
