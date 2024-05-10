e51f835ad8b0ba48254b40d232ca9d7a62cc4cc2
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-10 07:29:07 +0530

    feat: Add initial user data to Users component
    This commit adds initial user data to the Users component in the frontend. The users array is populated with two objects containing the ID and name of each user. This data will be used to populate the table in the Users component.

--- frontend/src/Users.jsx
+++ frontend/src/Users.jsx
@@ -1,5 +1,16 @@
 import UserForm from "./UserForm";
 
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+  },
+];
+
 const User = () => {
   return (
     <>
