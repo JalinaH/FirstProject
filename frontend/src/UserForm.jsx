4879eaf71dd651fd8e8523dbe84e6b736bdbee4a
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-10 06:45:23 +0530

    chore: Refactor UserForm component
    Refactor the UserForm component in the frontend to use the Material-UI Grid component for better layout and styling.

--- frontend/src/UserForm.jsx
+++ frontend/src/UserForm.jsx
@@ -1,9 +1,22 @@
-const UserForm = props => {
-    return(
-        <>
+import React from "react";
+import { Grid } from "@mui/material";
+
+const UserForm = (props) => {
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        backgroundColor: "white",
+        marginBottom: "30px",
+        display: "block",
+      }}
+    >
+        <Grid item xs={12} sm={6}>
             
-        </>
-    );
+        </Grid>
+    </Grid>
+  );
 };
 
 export default UserForm;
