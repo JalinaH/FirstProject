5134461dac9b12cb3d3d360d4a636e684f41a3c6
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-16 00:39:45 +0530

    refactor: Update AddUser page styling and form validation

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
 
 const HomePage = () => {
     const navigate = useNavigate();
 
     return (
-        <>
-        <h1>Home</h1>
-        <button onClick={() => navigate("/users")}>Users</button>
-        </>
+      <>
+        <h1>Sample User Management System</h1>
+        <p>
+          Welcome to my MERN-based web application! This platform offers
+          seamless CRUD (Create, Read, Update, Delete) operations, empowering
+          you to effortlessly manage user data. Whether you're adding new users,
+          retrieving existing ones, or updating and deleting records, this
+          intuitive interface ensures a smooth experience throughout. Dive in
+          and explore the simplicity and efficiency of user-friendly
+          application today!
+        </p>
+
+        <h2>Navigation</h2>
+        <div className="buttons">
+          <button onClick={() => navigate("/users")}>Users</button>
+          <br />
+          <button onClick={() => navigate("/users/adduser")}>Add User</button>
+        </div>
+      </>
     );
 };
 
