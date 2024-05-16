77d511cd293aa8511538ca28ee3d49833fc99196
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-16 17:45:56 +0530

    feat: Add endpoint for creating new users
    The code changes in this commit add a new endpoint `/users` to the backend server. This endpoint allows clients to send a POST request with user data to create a new user. The user data includes fields for ID, first name, last name, email, date of birth, and account type. If the user creation is successful, the server responds with a JSON object containing the newly created user. If there is an error, the server responds with a JSON object containing an error message. This addition enhances the functionality of the application by enabling the creation of new users.

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,23 @@ app.get("/users", async (req, res) => {
   }
 });
 
+app.post("/users", async (req, res) => {
+  const user = new User({
+    id: req.body.id,
+    f_name: req.body.f_name,
+    l_name: req.body.l_name,
+    email: req.body.email,
+    dob: req.body.dob,
+    account_type: req.body.account_type,
+  });
 
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
