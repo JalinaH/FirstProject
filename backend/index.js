b7ada7e9d9669e5535d05c6b4ad619d7c211ce53
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-16 18:21:35 +0530

    refactor: Update backend/index.js to use environment variables for port and host

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 5000 || process.env.PORT;
-const host = "localhost" || process.env.HOST;
+const host = "0.0.0.0" || process.env.HOST;
 
 require("dotenv").config();
 const User = require("./model");
