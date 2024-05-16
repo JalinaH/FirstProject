ea628412ef0e44b4426f25aa901b5220aa8c5722
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-16 17:18:11 +0530

    chore: Add endpoint for retrieving users

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,20 @@ const User = require("./model");
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+  res.send("Hello World");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 
 
 app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
