a4d78d7f9f52f1ccaf296136bd8d04f7fbeee1b2
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-17 21:24:12 +0530

    refactor: Update routing in App component
    This commit updates the routing in the App component to use the react-router-dom's BrowserRouter, Routes, and Route components. It adds routes for the HomePage, UserDashBoard, and AddUser components, allowing for navigation between different pages. This change improves the organization and maintainability of the codebase by separating the routing logic from the main component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
+import UserDashBoard from "./Pages/UserDashBoard/UserDashBoard";
+import AddUser from "./Pages/AddUser/AddUser";
 
 function App() {
-  const navigate = useNavigate();
 
   return (
     <>
-      <div>
-        <h1>Welcome to my webpage</h1>
-        <button className="user-button" onClick={() => navigate("/users")}>
-          Users
-        </button>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/users" element={<UserDashBoard />} />
+          <Route path="/users/adduser" element={<AddUser />} />
+          <Route path="/users/adduser/:id" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserDashBoard from "./Pages/UserDashBoard/UserDashBoard.jsx";
-import AddUser from "./Pages/AddUser/AddUser.jsx";
-import HomePage from "./Pages/HomePage/HomePage.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/users" element={<UserDashBoard />} />
-      <Route path="/users/adduser" element={<AddUser />} />
-      <Route path="/users/adduser/:id" element={<AddUser />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
